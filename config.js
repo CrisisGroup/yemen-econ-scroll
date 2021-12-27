b7b77307c1ac7bca7655f1dd185baf396776cd54
Paul Franz <paulkfranz@gmail.com>
2021-12-27 16:26:00 -0500

    hard refresh on mb style

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 var config = {
-    style: 'mapbox://styles/daltonwb/ckwlgtu7w2ojs14uqx5lztnq0',
+    style: 'mapbox://styles/daltonwb/ckxp6vvno0ebn14m58uyfh2v7',
     accessToken: 'pk.eyJ1IjoiZGFsdG9ud2IiLCJhIjoiOWdSSXFQSSJ9.HZyjh4g3TAAOAncwelv9Vw',
     showMarkers: false,
     markerColor: '#3FB1CE',
